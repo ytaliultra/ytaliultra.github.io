a3d03bc4ba4d06bebdf607488b555c37986eb99b
ytaliultra <118834976+ytaliultra@users.noreply.github.com>
2022-11-22 21:25:47 +0330

    Delete mailer directory

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'aliultra Personal Template'"; //letter subject
-	$to = 'aliultra@gmail.com'; // Enter Your E-mail
-	$from = 'alirezashaemi85@gmail.com'; // Admin e-mail
-	$fromName = 'aliultra-site'; // Your company name
-	$charset = 'UTF-8';
-?>
